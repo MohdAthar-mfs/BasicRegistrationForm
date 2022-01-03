4a0b4874e459da4ab93d354e26989f4ce476b12c
mfsi-MohdAthar <mohd.athar@mindfiresolutions.com>
2022-01-03 10:26:26 +0530

    commit on jquery-bootstrap

--- script.js
+++ script.js
@@ -1,53 +1,84 @@
 var Table = document.getElementById("DataTable");
 
-document.getElementById("Form").onsubmit = function (e) {
+$("#Form").on('submit', function (e) {
     e.preventDefault();
     myFunction();
-};
+});
 
 function myFunction() {
-    var FName = document.getElementById("FName").value;
-    var LName = document.getElementById("LName").value;
-    if (document.getElementById("Male").checked == true) {
+
+    if ($("#SubmitButton").val() == "Update") {
+        updateRow();
+        return false;
+    }
+    var FName = $("#FName").val();
+    var LName = $("#LName").val();
+    if ($("#Male").prop("checked") == true) {
         var Gender = "Male";
-    } else if (document.getElementById("Female").checked == true) {
+    } else if ($("#Female").prop("checked") == true) {
         var Gender = "Female";
     }
-    var DOB = document.getElementById("DOB").value;
+    var DOB = $("#DOB").val();
     //DOB Validation
     var d = DOB.split("-");
-    var date = new Date(d[0],d[1],d[2]);
+    var date = new Date(d[0], d[1], d[2]);
     var minDate = new Date;
     minDate.setFullYear(minDate.getFullYear() - 18);
     if (date > minDate) {
         alert("Date must be before " + minDate);
         return false;
     }
-    var Department = document.getElementById("Department").value;
-    var Email = document.getElementById("Email").value;
-    var Phone = document.getElementById("Phone").value;
+    var Department = $("#Department").val();
+    var Email = $("#Email").val();
+    var Phone = $("#Phone").val();
     //Phone Number validation using RegExp
     var phoneno = /^\d{10}$/;
     if (Phone.match(phoneno) == null) {
         alert("Phone number must be 10 digits.");
         return false;
     }
-    
-    var row = Table.insertRow(-1);
 
-    var col1 = row.insertCell(0);
-    var col2 = row.insertCell(1);
-    var col3 = row.insertCell(2);
-    var col4 = row.insertCell(3);
-    var col5 = row.insertCell(4);
-    var col6 = row.insertCell(5);
-    var col7 = row.insertCell(6);
+    $("#Form")[0].reset();
 
-    col1.innerHTML = FName;
-    col2.innerHTML = LName;
-    col3.innerHTML = Gender;
-    col4.innerHTML = DOB;
-    col5.innerHTML = Department;
-    col6.innerHTML = Email;
-    col7.innerHTML = Phone;
+    $("#DataTable tbody").append("<tr>" +
+        "<td>" + FName + "</td>" +
+        "<td>" + LName + "</td>" +
+        "<td>" + Gender + "</td>" +
+        "<td>" + DOB + "</td>" +
+        "<td>" + Department + "</td>" +
+        "<td>" + Email + "</td>" +
+        "<td>" + Phone + "</td>" +
+        "<td>" +
+        "<Button type='button' onclick='deleteRow(this);' class='btn btn-sm'>" +
+        "<span class='material-icons'>delete</span>" +
+        "<Button type='button' onclick='editRow(this);' class='btn btn-sm'>" +
+        "<span class='material-icons'>edit</span>" +
+        "</Button>" +
+        "</Button>" +
+        "</td>" +
+        "</tr>");
+}
+
+function deleteRow(ctl) {
+    $(ctl).parents("tr").remove();
+}
+
+var currentUser = null;
+
+function editRow(ctl) {
+    currentUser = $(ctl).parents("tr");
+    var cols = currentUser.children("td");
+    $("#FName").val($(cols[0]).text());
+    $("#LName").val($(cols[1]).text());
+    $("#Gender").val($(cols[2]).text());
+    $("#DOB").val($(cols[3]).text());
+    $("#Department").val($(cols[4]).text());
+    $("#Email").val($(cols[5]).text());
+    $("#Phone").val($(cols[6]).text());
+
+    $("#SubmitButton").attr("value","Update");
+}
+
+function updateRow() {
+    alert("Inside updateRow");
 }
